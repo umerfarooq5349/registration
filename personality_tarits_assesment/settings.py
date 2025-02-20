c0b6cb7901a6a38df4448ed0bb54308c1021c1d4
Umer Farooq <mumerfarooq557@gmail.com>
2025-02-20 17:06:55 +0500

    checking

--- personality_tarits_assesment/settings.py
+++ personality_tarits_assesment/settings.py
@@ -104,11 +104,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'templates' ,
+    BASE_DIR / 'templates/static',  # This should be where your static files are
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
